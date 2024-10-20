1ca5a47342f8c04f9f216dc88c263caedbecd33a
evan2chen <evan2.chen@gmail.com>
2024-10-20 19:30:08 -0400

    fixed small issue

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
-from flask import Flask, jsonify
-import requests
+
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -12,9 +12,15 @@ def empty():
 
 @app.route('/valassistant/positioning',methods = ['GET'])
 def positioning():
-    map = requests.args.get('map', default = None).lower()
-    gun = requests.args.get('weapon', default = None).lower()
-    rank = requests.args.get('rank', default = None).lower()
+    map = request.args.get('map', default = None).lower()
+    gun = request.args.get('weapon', default = None).lower()
+    rank = request.args.get('rank', default = None).lower()
+    if not map or not gun or not rank:
+        return jsonify({"error": "Missing parameters"}), 400
+    
+    map = map.lower()
+    gun = gun.lower()
+    rank = rank.lower()
 
     return f"You requested to see the positioning guide for {rank} players on {map} with {gun}"
 
